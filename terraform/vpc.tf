3e1b51f184e23bf00989eb1d3fcd109d4f8f4c37
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-01-14 20:47:18 -0300

    feat: adiciona arquivos terraform para configurar as vpc, subnets, nat gateway e etc

---
+++ terraform/vpc.tf
@@ -0,0 +1,113 @@
+// VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = "dev-main"
+  }
+}
+
+// INTERNET GATEWAY
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+  
+}
+
+// SUBNETS 
+
+resource "aws_subnet" "public_zone1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/19"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    "Name" = "dev-public-us-east-1a"
+  }
+}
+
+resource "aws_subnet" "private_zone1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.64.0/19"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    "Name" = "dev-private-us-east-1a"
+  }
+}
+
+// ROUTE TABLE PUBLIC
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+// ROUTE TABLE PRIVATE
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route  {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "dev-private"
+  }
+}
+
+// ROUTE TABLE ASSOCIATION PUBLIC
+
+resource "aws_route_table_association" "public_zone1" {
+  subnet_id = aws_subnet.public_zone1.id
+  route_table_id = aws_route_table.public.id  
+}
+
+// ROUTE TABLE ASSOCIATION PUBLIC
+
+resource "aws_route_table_association" "private_zone1" {
+  subnet_id = aws_subnet.private_zone1.id
+  route_table_id = aws_route_table.private.id  
+}
+
+
+// ELASTIC IP FOR NAT GATEWAY
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "dev-nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public_zone1.id
+
+  tags = {
+    Name = "dev-nat"
+  }
+
+  depends_on = [ aws_internet_gateway.igw ]
+}
+
