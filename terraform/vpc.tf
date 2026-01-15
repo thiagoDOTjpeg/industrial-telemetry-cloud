028f836b7ab196098170cef30e304eaae0930e53
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-01-14 21:04:22 -0300

    feat: adiciona mais 2 subnets public e privada, para o rds conseguir subir

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -21,6 +21,17 @@ resource "aws_internet_gateway" "igw" {
   
 }
 
+// DB SUBNETS GROUP
+
+resource "aws_db_subnet_group" "default" {
+  name = "main"
+  subnet_ids = [aws_subnet.private_zone1.id, aws_subnet.private_zone2.id]
+
+  tags = {
+    Name = "dev-db-subnet"
+  }
+}
+
 // SUBNETS 
 
 resource "aws_subnet" "public_zone1" {
@@ -34,14 +45,36 @@ resource "aws_subnet" "public_zone1" {
   }
 }
 
+resource "aws_subnet" "public_zone2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.32.0/19"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    "Name" = "dev-public-us-east-1b"
+  }
+}
+
 resource "aws_subnet" "private_zone1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.64.0/19"
   availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   
   tags = {
     "Name" = "dev-private-us-east-1a"
+  }
+}
+
+resource "aws_subnet" "private_zone2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.96.0/19"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+  
+  tags = {
+    "Name" = "dev-private-us-east-1b"
   }
 }
 
@@ -82,10 +115,20 @@ resource "aws_route_table_association" "public_zone1" {
   route_table_id = aws_route_table.public.id  
 }
 
+resource "aws_route_table_association" "public_zone2" {
+  subnet_id = aws_subnet.public_zone2.id
+  route_table_id = aws_route_table.public.id  
+}
+
 // ROUTE TABLE ASSOCIATION PUBLIC
 
 resource "aws_route_table_association" "private_zone1" {
   subnet_id = aws_subnet.private_zone1.id
+  route_table_id = aws_route_table.private.id  
+}
+
+resource "aws_route_table_association" "private_zone2" {
+  subnet_id = aws_subnet.private_zone2.id
   route_table_id = aws_route_table.private.id  
 }
 
