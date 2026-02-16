f2936795a6657f2e3954212b1582108fc5911151
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-02-16 19:07:42 -0300

    feat: adiciona conexão com o rds no lambda handler

--- lambda/lambda_handler.py
+++ lambda/lambda_handler.py
@@ -1,32 +1,59 @@
 import json
 import logging
+import psycopg2
+import boto3
+from config import AWS_REGION, DB_NAME, DB_ENDPOINT, DB_PORT, DB_USER
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def lambda_handler(event, context):
-    logger.info(f"Received event: {json.dumps(event)}")
-    
-    records = event.get("Records", [])
-    
-    if not records:
-        logger.warning("No Records found in event. Is this a test invocation?")
-        return {
-            'statusCode': 200,
-            'body': json.dumps('No messages to process')
-        }
-    
-    for record in records:
-        message_body = record.get("body", "")
-        logger.info(f"Processing message body: {message_body}")
+client = boto3.client('rds', region_name=AWS_REGION)
 
-        try:
-            message_data = json.loads(message_body)
-            logger.info(f"Message data: {message_data}")
-        except json.JSONDecodeError:
-            logger.info("Message body is not a JSON string, treating as plain text.")
+
+def get_auth_token():
+    return client.generate_db_auth_token(
+        DBHostname=DB_ENDPOINT,
+        Port=DB_PORT,
+        DBUsername=DB_USER,
+        Region=AWS_REGION
+    )
+
+
+
+def lambda_handler(event, context):
+    token = get_auth_token()
+    
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host=DB_ENDPOINT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=token,
+            port=DB_PORT,
+            connect_timeout=5,
+            sslmode='disable' 
+        )
         
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Messages processed successfully')
-    }
+        cur = conn.cursor()
+        
+        for record in event.get("Records", []):
+            message_data = json.loads(record["body"])
+            
+            query = "INSERT INTO telemetry (machine_id, temperature, status) VALUES (%s, %s, %s)"
+            cur.execute(query, (
+                message_data['machine_id'], 
+                message_data['temperature'], 
+                message_data['status']
+            ))
+            
+        conn.commit()
+        cur.close()
+        return {'statusCode': 200, 'body': json.dumps('Sucesso')}
+
+    except Exception as e:
+        logger.error(f"Erro na conexão ou execução: {e}")
+        raise e
+    finally:
+        if conn:
+            conn.close()
