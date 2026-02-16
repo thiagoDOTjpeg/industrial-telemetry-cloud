3c22ca9efe4714336778b01828e4e944e81bb325
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-02-16 19:27:50 -0300

    fix: arquivo config procurando env com nome errado

--- lambda/config.py
+++ lambda/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-DB_ENDPOINT = os.getenv("ENDPOINT", "localhost")
+DB_ENDPOINT = os.getenv("DB_ENDPOINT", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_USER = os.getenv("DB_USER", "admin")
 DB_NAME = os.getenv("DB_NAME", "postgres")
