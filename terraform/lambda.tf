35025123e8f9eeddad9b28929cb9d232a096bed1
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-02-17 20:24:26 -0300

    fix: mapeia porta pela variavel no terraform do lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -41,7 +41,7 @@ resource "aws_lambda_function" "lambda_handler" {
     variables = {
       AWS_REGION = var.aws_region
       DB_ENDPOINT   = aws_db_instance.main-db.address
-      DB_PORT    = "5432"
+      DB_PORT    = aws_db_instance.main-db.port
       DB_USER    = var.db_username
       DB_NAME    = "postgres" 
     }
