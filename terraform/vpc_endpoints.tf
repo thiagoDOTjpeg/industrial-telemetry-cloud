6c818b9ddc195fde99e6565eb0f3ca1a8399b74f
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-02-19 14:45:36 -0300

    feat: adiciona vpc endpoints

---
+++ terraform/vpc_endpoints.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "vpc_endpoints_sg" {
+  name   = "dev-vpc-endpoints-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lambda_sg.id]
+  }
+
+  tags = { Name = "dev-vpc-endpoints-sg" }
+}
+
+resource "aws_vpc_endpoint" "services" {
+  for_each = toset(["sqs", "secretsmanager", "sts"])
+
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.${each.key}"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private_zone1.id, aws_subnet.private_zone2.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
