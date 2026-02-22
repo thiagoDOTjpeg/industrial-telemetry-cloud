b373e1382bf15dce4a2cec7780f65e43530ce839
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-02-21 21:12:42 -0300

    remove: uso de config file para puxar direto na func

--- lambda/test_handler.py
+++
@@ -1,9 +0,0 @@
-from lambda_handler import lambda_handler
-import json
-
-mock_event = {
-    "Records": [{"body": json.dumps({"machine_id": "dev-01", "temperature": 25.5, "status": "OK"})}]
-}
-
-if __name__ == "__main__":
-    lambda_handler(mock_event, None)
