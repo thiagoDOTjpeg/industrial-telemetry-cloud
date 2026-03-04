25ca4939685af1e6a9777d92076c393926ba9906
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-03-04 12:53:31 -0300

    remove: deleta recursos desnecessários do terraform como public subnets, internet gateway e nat gateway

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,17 +10,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-// INTERNET GATEWAY
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "dev-igw"
-  }
-
-}
-
 // DB SUBNETS GROUP
 
 resource "aws_db_subnet_group" "default" {
@@ -29,30 +18,6 @@ resource "aws_db_subnet_group" "default" {
 
   tags = {
     Name = "dev-db-subnet"
-  }
-}
-
-// SUBNETS 
-
-resource "aws_subnet" "public_zone1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/19"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    "Name" = "dev-public-us-east-1a"
-  }
-}
-
-resource "aws_subnet" "public_zone2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.32.0/19"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    "Name" = "dev-public-us-east-1b"
   }
 }
 
@@ -78,46 +43,15 @@ resource "aws_subnet" "private_zone2" {
   }
 }
 
-// ROUTE TABLE PUBLIC
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "dev-public"
-  }
-}
 
 // ROUTE TABLE PRIVATE
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
   tags = {
     Name = "dev-private"
   }
-}
-
-// ROUTE TABLE ASSOCIATION PUBLIC
-
-resource "aws_route_table_association" "public_zone1" {
-  subnet_id      = aws_subnet.public_zone1.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_zone2" {
-  subnet_id      = aws_subnet.public_zone2.id
-  route_table_id = aws_route_table.public.id
 }
 
 // ROUTE TABLE ASSOCIATION PUBLIC
@@ -131,26 +65,3 @@ resource "aws_route_table_association" "private_zone2" {
   subnet_id      = aws_subnet.private_zone2.id
   route_table_id = aws_route_table.private.id
 }
-
-
-// ELASTIC IP FOR NAT GATEWAY
-
-resource "aws_eip" "nat" {
-  domain = "vpc"
-
-  tags = {
-    Name = "dev-nat"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_zone1.id
-
-  tags = {
-    Name = "dev-nat"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
