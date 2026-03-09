1bd951d3d826246626cdb7b6f25d90308e728129
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2026-03-09 18:53:30 -0300

    feat: adiciona backend s3 para armazenar o tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,20 @@ provider "aws" {
   region                      = var.aws_region
 }
 
+
+
 terraform {
   required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket = "terraform-state"
+    key = "industrial-telemetry/terraform.tfstate"
+    region = "us-east-1"
+    endpoint = "http://localhost:4566"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
